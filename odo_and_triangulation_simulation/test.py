71067e167803c84b662f2170a03fa373bce01b77
Alkanoor <juju.schou@gmail.com>
2017-06-05 13:35:46 +0200

    test final

--- odo_and_triangulation_simulation/test.py
+++ odo_and_triangulation_simulation/test.py
@@ -23,8 +23,9 @@ def relative_noice(relative, x):
 def mixed(relative, absolute, sigma_relative, sigma_absolute, c=None):
     phi = sigma_absolute/sigma_relative
     if c is None:
-        c = phi/(2*phi+1)
+        c = phi/(phi+1)
         c = 0.69
+        #c = sigma_absolute/(sigma_absolute+sigma_relative)
     r = [absolute[0]]
     for i in range(1,len(relative)):
         r.append((1-c)*absolute[i]+c*relative[i]+c*r[i-1])
@@ -34,31 +35,51 @@ def simu_get_position_codeuse_et_triangulation(relative, absolute, sigma_relativ
     simu = absolute[:]
 
     for i in range(1, len(absolute)):
-        simu[i] = (sigma_relative * absolute[i] + sigma_absolute * relative[i] + simu[i-1]) / (sigma_absolute + sigma_relative)
+        simu[i] = (sigma_relative * absolute[i] + sigma_absolute * (relative[i] + simu[i-1])) / (sigma_absolute + sigma_relative)
 
     return simu
 
 N = 500
 t = np.arange(0, N, 1)
-x_base = [float(i)/5.0 for i in range(N//2)]
-x_base.extend([float(N//2-1-i)/5.0 for i in range(N//2)])
+x_base = [float(i)/2.0 for i in range(N//2)]
+x_base.extend([float(N//2-1-i)/2.0 for i in range(N//2)])
 
 sigma_relative = 1.4
-sigma_absolute = 4.0
-odometry_error = generate_gaussian(0, sigma_relative, N)
-odometry = relative_noice(odometry_error, x_base)
-beacon = gaussian_noice_added(x_base, sigma_absolute)
-corrected_ju = mixed(odometry_error, beacon, sigma_relative, sigma_absolute)
-corrected_nico = simu_get_position_codeuse_et_triangulation(odometry_error, beacon, sigma_relative, sigma_absolute)
+sigma_absolute = 5.0
+
+mean_ju = []
+mean_nico = []
+std_ju = []
+std_nico = []
+for i in range(1000):
+    beacon = gaussian_noice_added(x_base, sigma_absolute)
+    odometry_error = generate_gaussian(0, sigma_relative, N)
+    odometry_measured = np.concatenate((np.array([0.0]), np.array(x_base[1:])-np.array(x_base[:-1])))+np.array(odometry_error)
+    odometry = relative_noice(odometry_error, x_base)
+
+    corrected_ju = mixed(odometry_measured, beacon, sigma_relative, sigma_absolute)
+    corrected_nico = simu_get_position_codeuse_et_triangulation(odometry_measured, beacon, sigma_relative, sigma_absolute)
+
+    mean_ju.append(abs((np.array(x_base)-np.array(corrected_ju)).mean()))
+    mean_nico.append(abs((np.array(x_base)-np.array(corrected_nico)).mean()))
+    std_ju.append((np.array(x_base)-np.array(corrected_ju)).std())
+    std_nico.append((np.array(x_base)-np.array(corrected_nico)).std())
+
+print("Mean of mean error and deviation (julien) : "+str(np.array(mean_ju).mean())+" "+str(np.array(std_ju).mean()))
+print("Mean of mean error and deviation (nico) : "+str(np.array(mean_nico).mean())+" "+str(np.array(std_nico).mean()))
+
 
 plt.figure(1)
 plt.plot(t, beacon, 'k', color='cyan')
 plt.plot(t, odometry, 'k', color='red')
+plt.plot(t, odometry_measured, 'k', color='grey')
 plt.plot(t, x_base, 'k', color='black')
 plt.plot(t, corrected_ju, 'k', color='green')
 plt.plot(t, corrected_nico, 'k', color='orange')
 plt.show()
 
+print("")
+print("And result on last stage :")
 print("Mean error and standard deviation beacon : "+str(abs((np.array(x_base)-np.array(beacon)).mean()))+" "+str((np.array(x_base)-np.array(beacon)).std()))
 print("Mean error and standard deviation odometry : "+str(abs((np.array(x_base)-np.array(odometry)).mean()))+" "+str((np.array(x_base)-np.array(odometry)).std())+" and just the relative error : "+str(abs(np.array(odometry_error).mean()))+" "+str(np.array(odometry_error).std()))
 print("Mean error and standard deviation corrected (julien) : "+str(abs((np.array(x_base)-np.array(corrected_ju)).mean()))+" "+str((np.array(x_base)-np.array(corrected_ju)).std()))
